7a0808bff198ef2213a93f198a1132b7c0b104be
Jogchum1 <jogchum1@hotmail.nl>
2022-03-30 22:00:33 +0200

    Werkt nu soort van op audio

--- ProceduralArt/Assets/PlanetAudio.cs
+++ ProceduralArt/Assets/PlanetAudio.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+
 
 public class PlanetAudio : MonoBehaviour
 {
     public AudioPeer audioPeer;
     public NoiseSettings noise;
-    public Planet planet;
+    public GameObject planet;
+
+
+    
     
     // Update is called once per frame
     void Update()
     {
-        planet.autoUpdate = true;
-        planet.shapeSettings.noiseLayers[1].noiseSettings.roughness =+ 0.01f;
+        //planet.autoUpdate = true;
+        //planet.shapeSettings.noiseLayers[1].noiseSettings.roughness =+ 0.01f;
         //noise.roughness += Time.deltaTime * audioPeer._audioBand[1];
         //noise.roughness += Time.deltaTime * audioPeer._audioBand[1];
+        planet.GetComponent<Planet>().shapeSettings.noiseLayers[0].noiseSettings.roughness += audioPeer._audioBand[1];
+        planet.GetComponent<Planet>().OnColourSettingsUpdated();
+        planet.GetComponent<Planet>().OnShapeSettingsUpdated();
+
+        planet.GetComponent<Planet>().autoUpdate = true;
     }
 }
